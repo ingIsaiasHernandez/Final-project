bdc12a06fc6cf37613489042b225537b17e0c246
Angel Herrera Zermeño <angelherrera1996@gmail.com>
2023-03-26 15:40:44 -0600

    added initModels

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,6 +1,8 @@
 const Cart = require("./cart.model");
 const Order = require("./order.model");
 const Products = require("./product.models");
+const ProductInCart = require("./productInCart.model");
+const ProductInOrder = require("./productInOrder.model");
 const Users = require("./user.model");
 
 const initModels = () => {
@@ -13,7 +15,17 @@ const initModels = () => {
     Order.belongsTo(Users, {foreignKey: "userId"});
     Users.hasMany(Order, {foreignKey: "userId"});
 
-    
+    Cart.belongsTo(ProductInCart, {foreignKey: "cartId"});
+    ProductInCart.hasMany(Cart, {foreignKey: "cartId"});
+
+    Products.belongsTo(ProductInCart, {foreignKey: "productId"});
+    ProductInCart.hasMany(Products, {foreignKey: "productId"});
+
+    Order.belongsTo(ProductInOrder, {foreignKey: "orderId"});
+    ProductInOrder.hasMany(Order, {foreignKey: "orderId"});
+
+    Products.belongsTo(ProductInOrder, {foreignKey: "productId"});
+    ProductInOrder.hasMany(Products, {foreignKey: "productId"});
 }
 
 module.exports = initModels;
